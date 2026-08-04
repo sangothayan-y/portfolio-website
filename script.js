6fa58f75ff91e21f01f1ed7ebad1bd9d2a6a4935
Sangothayan Y <65010078+sangothayan-y@users.noreply.github.com>
2026-08-04 23:01:50 +0530

    add photo and effect

---
+++ script.js
@@ -0,0 +1,39 @@
+(function(){
+    const card=document.getElementById('title');
+    const container=document.querySelector('#pic');       
+
+    const max_rotation=18;
+
+    function handleMouseMove(e){
+        const rect=container.getBoundingClientRect();
+        let mouseX=(e.clientX-rect.left)/rect.width;
+        let mouseY=(e.clientY-rect.top)/rect.height;
+        mouseX=Math.min(1,Math.max(0,mouseX));
+        mouseY=Math.min(1,Math.max(0,mouseY));
+
+        const rotateY=(mouseX -0.5)*2* max_rotation;
+        const rotateX=( 0.5-mouseY)*2* max_rotation;
+        card.style.transform=`perspective(1200px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+        const shadowX=(mouseX -0.5)*20;
+        const shadowY=( 0.5-mouseY)*15;
+        card.style.boxShadow=`${shadowX}px ${shadowY}px 40px -12px rgba(0,0,0,0.5)`;
+    }
+    function restcard(){
+        card.style.transform='perspective(1200px) rotateX(0deg) rotateY(0deg) translateZ(0px)';
+        card.style.boxShadow='0 25px 40px -12px rgba(0,0,0,0.5)';
+    }
+
+    container.addEventListener('mousemove',handleMouseMove);
+    container.addEventListener('mouseout',restcard);
+
+    container.addEventListener('touchmove',(e)=>{
+        if(e.touches.length){
+           const touch=e.touches[0];
+           const fakevebt={clintX:touch.clientX,clientY:touch.clientY};
+           handleMouseMove(fakevebt);
+        }
+
+    });
+    container.addEventListener('touchend',restcard);
+})();
